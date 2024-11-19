30f15f89c813fd70fe217443b347c0ee635f48ae
diego maneyro <diegomaneyro@gmail.com>
2024-11-19 15:42:34 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   app.py
    	new file:   requirements.txt
    	new file:   static/css/styles.css
    	deleted:    static/style.css
    	modified:   templates/index.html
    	modified:   templates/result.html

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, string, secrets
+from flask import Flask, render_template, request
+import string 
+import secrets
 
 # Instancia de Flask en la app
 app = Flask(__name__)
 
 # Funcion que genera la contraseña
-def generar_contraseña():
-    caracter = string.ascii_leters + string.digits + string.punctuation
-    contraseña = ''.join(secrets.choice(caracter)) for _ in range(longitud))
+def generar_contraseña(longitud=12):
+    caracter = string.ascii_leters + string.digits + string.puntuation
+    contraseña = ''.join(secrets.choice(caracter) for _ in range(longitud))
     return contraseña     
 
 # Ruta de la raiz de la app
@@ -14,3 +16,9 @@ def generar_contraseña():
 async def index():
     return render_template("index.html")
 
+# Ruta generar contraseña
+@app.route("/generar", methods={"POST"})
+async def generar():
+    longitud = int(request.form.get('longitud', 12))
+    contraseña = generar_contraseña('longitud')
+    return render_template('result.html', contraseña=contraseña)
